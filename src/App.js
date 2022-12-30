487dfabc8466b7657a22c3d04d07107ea86d28fc
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-30 16:46:29 -0700

    added route for home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
       </Router>
     </>
   );
