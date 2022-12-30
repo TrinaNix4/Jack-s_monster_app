36a3dbe9464a38a80bcdb175a284fbdb659c0b15
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-30 16:47:00 -0700

    created herosection page

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import "../App.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="https://youtu.be/kni0C70947Y" autoPlay loop muted />
+      <h1>Horror awaits</h1>
+      <p>Are you scared? </p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Get Started
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Watch Trailer <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
