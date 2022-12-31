6bc38f876043ef694647330984041b74e9636fb2
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-30 18:40:10 -0700

    removed extra button

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,22 +7,15 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="https://youtu.be/kni0C70947Y" autoPlay loop muted />
-      <h1>Horror awaits</h1>
+      <h1>Horror awaits...</h1>
       <p>Are you scared? </p>
       <div className="hero-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Get Started
-        </Button>
         <Button
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          Watch Trailer <i className="far fa-play-circle" />
+          Play Game <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
