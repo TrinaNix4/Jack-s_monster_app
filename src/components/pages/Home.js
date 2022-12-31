b5231f1b41c354fc4ad86c892c287188fe5f867a
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-30 18:40:55 -0700

    added embedded video

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,22 @@
 import "../../App.css";
 import HeroSection from "../HeroSection";
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Home() {
   return (
-    <>
-      <HeroSection />
-    </>
+    <div>
+      <Container>
+        <HeroSection />
+        <div className="ratio ratio-16x9">
+          <iframe
+            src="https://www.youtube.com/embed/XVENjm3Bjk0"
+            title="YouTube video"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </Container>
+    </div>
   );
 }
 
