23fe1a915ce07e6b47da1a23671efaf3bcab40f2
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-31 15:28:02 -0700

    added monsters function

--- src/components/pages/Monsters.js
+++ src/components/pages/Monsters.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "../../App.css";
+import Cards from "../Cards";
 
-export default function Services() {
-  return <h1 className="services">SERVICES</h1>;
+function Monsters() {
+  return (
+    <h1>
+      {" "}
+      <Cards />
+      All about the Monsters
+      <h2>Huggy-wuggy</h2>
+      <h2>Boxy-boo</h2>
+      <h2>Poppy</h2>
+    </h1>
+  );
 }
+
+export default Monsters;
