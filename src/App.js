78603750382116b1f6ed4d1033dd23481eae3f20
Trina Nixon <TrinaNixon7@gmail.com>
2022-12-30 18:39:17 -0700

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
