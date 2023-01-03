f0f114fda0db331a9564da4eb56d2aeafc148c44
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-03 16:29:40 -0700

    edited footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,83 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button } from "./Button";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer-container">
       <section className="footer-subscription">
-        <p className="footer-subscription-heading">Join the adventure...</p>
-        <p className="footer-subscription-text">
-          You can subscribe at any time
-        </p>
+        <p className="footer-subscription-heading">Join the Adventure!</p>
         <div className="input-areas">
           <form>
             <input
-              type="email"
-              name="email"
-              placeholder="Your email"
               className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
             />
             <Button buttonStyle="btn--outline">Subscribe</Button>
           </form>
+        </div>
+      </section>
+      <div class="footer-links">
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div class="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Youtube</Link>
+          </div>
+        </div>
+      </div>
+      <section class="social-media">
+        <div class="social-media-wrap">
+          <div class="footer-logo">
+            <Link to="/" className="social-logo">
+              the playhouse
+              <i class="fab fa-typo3" />
+            </Link>
+          </div>
+          <small class="website-rights">the playhouse © 2022</small>
+          <div class="social-icons">
+            <Link
+              class="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i class="fab fa-instagram" />
+            </Link>
+            <Link
+              class="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i class="fab fa-youtube" />
+            </Link>
+
+            <Link
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i class="fab fa-linkedin" />
+            </Link>
+          </div>
         </div>
       </section>
     </div>
