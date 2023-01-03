31d08c00a5640a8f1557a083446afbd8f6dbd014
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-03 16:30:29 -0700

    removed video from hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,6 @@ import "../App.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="https://youtu.be/kni0C70947Y" autoPlay loop muted />
       <h1>Horror awaits...</h1>
       <p>Are you scared? </p>
       <div className="hero-btns">
